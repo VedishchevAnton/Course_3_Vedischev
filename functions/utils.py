67da4d711331908c35809a5b13b20318a0c3b3ba
Anton Vedischev <vedishchevanton@gmail.com>
2023-02-23 14:11:02 +0300

    добавлена функция получения данных из json файла

--- functions/utils.py
+++ functions/utils.py
@@ -3,3 +3,10 @@ import datetime
 from data_link import DATA_SOURCE
 
 
+def load_json():
+    """
+    Получение данных из json файла
+    """
+    data = requests.get(DATA_SOURCE)
+    data_words = data.json()
+    return data_words
