dfb6792f577252eb66c1d0595c743d28f4e5d1b7
Anton Vedischev <vedishchevanton@gmail.com>
2023-02-23 14:12:21 +0300

    добавлена функция получения списка выполненных операций

--- functions/utils.py
+++ functions/utils.py
@@ -10,3 +10,18 @@ def load_json():
     data = requests.get(DATA_SOURCE)
     data_words = data.json()
     return data_words
+
+def state_executed():
+    """
+    Функция получения списка выполненных операций
+    """
+    executed_data = load_json()
+    list_executed = []
+    for i in executed_data:
+        if not i:
+            continue
+        else:
+            if i['state'] == "EXECUTED":
+                list_executed.append(i)
+
+    return list_executed
