8c89793fa387cd3fe36f695a571122b328a0b804
Anton Vedischev <vedishchevanton@gmail.com>
2023-02-24 13:16:21 +0300

    изменение data_link.py

--- data_link.py
+++ data_link.py
@@ -1,3 +1,1 @@
-DATA_SOURCE = "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb" \
-              "-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp" \
-              "=1677228615708&signature=ePRDSh4AECWnjJ3-xgSr500AYcZetQFiUs8oVELtyzY&downloadName=operations.json"
+DATA_SOURCE = 'https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1677320049623&signature=kU6Aay0no8EWL9m7tCdLwgRbjWQuiaeMGCI2iaczSFk&downloadName=operations.json'
